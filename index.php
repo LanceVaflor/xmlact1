3ab17149d76efc80c1b71d3b24248a2870f00796
Lance <60122560+LanceVaflor@users.noreply.github.com>
2020-01-28 11:57:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,21 @@
        $domOBJ->load("https://xmlact1vaflor.herokuapp.com/rss.php");//XML page URL
 
        $content = $domOBJ->getElementsByTagName("Monster");
+?>
     
        
        <h1>Monsters</h1>
            
        
         <?php
+              foreach($content as $data)
+        {          
            $Monster = $data->getElementsByTagName("Monster")->item(0)->nodeValue;
            $Classification = $data->getElementsByTagName("Classification")->item(0)->nodeValue;
            $Immunity = $data->getElementsByTagName("Immunity")->item(0)->nodeValue;
            $Tactics = $data->getElementsByTagName("Tactics")->item(0)->nodeValue;
            $Occurence = $data->getElementsByTagName("Occurence")->item(0)->nodeValue;
-           echo "<ul><h2>$Monster - $type</h2>
+           echo "<ul><h2>$Monster</h2>
                     <ul>
                         <li>Classification: $Classification </li>
                         <li>Immunity: $Immunity</li>
@@ -23,6 +26,7 @@
                         
                     </ul>
                 </ul>";
+              }
 ?>
     
 
